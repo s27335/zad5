b00d391f7cbd5dcce7440be10468e3777d363177
es27335 <s27335@pjwstk.edu.pl>
2024-04-08 12:55:28 +0200

    animals added

---
+++ WebApp/WebApp/Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models;
+
+public class Animal
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string category { get; set; }
+    public double weight { get; set; }
+    public string colour { get; set; }
+    
+}
--- WebApp/WebApp/Program.cs
+++ WebApp/WebApp/Program.cs
@@ -1,3 +1,5 @@
+using WebApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+var _animals = new List<Animal>()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Animal{id =1,name = "",category ="dog",weight = 15.4, colour = "black"},
+    new Animal{id = 2, name = "",category = "cat", weight = 5.9,colour = "white"},
+    new Animal{id = 3, name = "", category = "dog", weight = 10.2, colour = "brown"}
 };
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
+app.MapGet("/api/animals", () => Results.Ok(_animals))
+    .WithName("Animals")
     .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
